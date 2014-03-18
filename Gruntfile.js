62b0bca9d3bc62d675a1e1ed0b16e1e9b503671f
Cengiz Ulusoy <culusoy@ask-cs.com>
2014-03-18 15:03:15 +0100

    changed port for server

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: 3000,
                 livereload: 35729,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: '0.0.0.0' //'localhost'
